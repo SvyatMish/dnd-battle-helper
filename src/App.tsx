b5a0d02cb049d4697a1059f232386caa639f7239
SvyatMish <svyatoslav.mish@yandex.ru>
2026-06-20 19:52:14 +0300

    handle redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import { Routes, Route, Link } from 'react-router-dom'
+import {Routes, Route, Link, useLocation, useNavigate} from 'react-router-dom'
+import {useEffect} from "react";
 
 function Home() {
-  return <h1>Home12</h1>
+    return <h1>Home12</h1>
 }
 
 function Battle() {
-  return <h1>Battle1</h1>
+    return <h1>Battle1</h1>
 }
 
 function App() {
-  return (
-      <>
-        <nav>
-          <Link to="/">Home</Link> | <Link to="/battle">Battle</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/battle" element={<Battle />} />
-        </Routes>
-      </>
-  )
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (location.search) {
+            const searchParams = new URLSearchParams(location.search);
+            console.log(searchParams);
+            const redirectUrl = searchParams.get('redirectUrl');
+            console.log('redirectUrl', redirectUrl);
+            if (redirectUrl) {
+                navigate(redirectUrl, {replace: true});
+            }
+        }
+    }, [])
+
+    return (
+        <>
+            <nav>
+                <Link to="/">Home</Link> | <Link to="/battle">Battle</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/battle" element={<Battle/>}/>
+            </Routes>
+        </>
+    )
 }
 
 export default App
