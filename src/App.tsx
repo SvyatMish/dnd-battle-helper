2e3b3154b9f9b91a7de78e44453fad7a9dbfd81f
SvyatMish <svyatoslav.mish@yandex.ru>
2026-06-20 20:01:16 +0300

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,45 @@
-import {Routes, Route, Link, useLocation, useNavigate} from 'react-router-dom'
-import {useEffect} from "react";
+import {
+  Routes,
+  Route,
+  Link,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import { useEffect } from "react";
 
 function Home() {
-    return <h1>Home12</h1>
+  return <h1>Home12</h1>;
 }
 
 function Battle() {
-    return <h1>Battle1</h1>
+  return <h1>Battle1</h1>;
 }
 
 function App() {
-    const location = useLocation();
-    const navigate = useNavigate();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (location.search) {
-            const searchParams = new URLSearchParams(location.search);
-            const redirectUrl = searchParams.get('redirectUrl');
-            if (redirectUrl) {
-                navigate(redirectUrl, {replace: true});
-            }
-        }
-    }, [])
+  useEffect(() => {
+    if (location.search) {
+      const searchParams = new URLSearchParams(location.search);
+      const redirectUrl = searchParams.get("redirectUrl");
+      if (redirectUrl) {
+        navigate(redirectUrl, { replace: true });
+      }
+    }
+  }, []);
 
-    return (
-        <>
-            <nav>
-                <Link to="/">Home</Link> | <Link to="/battle">Battle</Link>
-            </nav>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/battle" element={<Battle/>}/>
-            </Routes>
-        </>
-    )
+  return (
+    <>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/battle">Battle</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/battle" element={<Battle />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
