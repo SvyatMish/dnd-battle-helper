e9d5e59b2feeed432625f304bd12f1a6341a65a8
SvyatMish <svyatoslav.mish@yandex.ru>
2026-07-08 23:12:30 +0300

    feat: add bestiary search

--- src/components/monster-list.tsx
+++ src/components/monster-list.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 import { type Monster } from "../types/bestiary.ts";
 import { MonsterPanel } from "./monster-panel.tsx";
@@ -6,14 +6,24 @@ import { MonsterPanel } from "./monster-panel.tsx";
 export const MonsterList: React.FC<{
   monsters: Monster[];
   isLoading: boolean;
-}> = ({ monsters, isLoading }) => {
+  search?: string;
+}> = ({ monsters, isLoading, search }) => {
+  const filtered = useMemo(() => {
+    if (!search) {
+      return monsters;
+    }
+    return monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(search.toLowerCase());
+    });
+  }, [search]);
+
   if (isLoading) {
     return "Loading...";
   }
 
   return (
-    <div className="flex space-y-2 space-x-2 flex-wrap">
-      {monsters.map((item) => (
+    <div className="flex flex-wrap space-y-2 space-x-2">
+      {filtered.map((item) => (
         <MonsterPanel key={item.id} monster={item} currentMonsters={monsters} />
       ))}
     </div>
--- src/pages/bestiary-page.tsx
+++ src/pages/bestiary-page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { TextField } from "@mui/material";
 
 import { MonsterList } from "../components/monster-list.tsx";
 import { AddMonsterForm } from "../components/add-monster-form.tsx";
@@ -7,12 +8,33 @@ import { useGetBestiary } from "../queries/bestiary.ts";
 
 export const BestiaryPage: React.FC = () => {
   const bestiaryQuery = useGetBestiary();
+  const [search, setSearch] = useState("");
+
+  const handleSearch = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  }, []);
 
   return (
-    <SidebarLayout sidebarSlot={null}>
+    <SidebarLayout
+      searchSlot={
+        <div>
+          <TextField
+            id="search"
+            label="Поиск"
+            fullWidth
+            variant="outlined"
+            size="medium"
+            type="text"
+            value={search}
+            onChange={handleSearch}
+          />
+        </div>
+      }
+    >
       <div className="mt-2 space-y-4 p-5">
         <AddMonsterForm currentMonsters={bestiaryQuery.data || []} />
         <MonsterList
+          search={search}
           monsters={bestiaryQuery.data || []}
           isLoading={bestiaryQuery.isLoading}
         />
